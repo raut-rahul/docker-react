cc83971661522c81be018588ab44e0827cbbb9c9
Rahul Raut <RRaut@AffinionDS.Com>
2021-08-22 09:32:43 +0530

    change in app.js file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This should work now!
         </a>
       </header>
     </div>
